850c777b741ee457689a0226dbe9ac066aae797e
Tim Gundmann <tim@gundmann.dk>
2019-03-01 08:16:48 +0100

    div updates

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,8 @@
+import { AuthService } from './../services/auth.service';
 import { UserService } from './../services/user.service';
 import { Component, OnInit } from '@angular/core';
 import { NotificationService } from 'app/services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -10,12 +12,20 @@ import { NotificationService } from 'app/services';
 
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private rotuer: Router, private authService: AuthService) { }
 
   ngOnInit() { }
 
   scrollToAignUp(element) {
     element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+  }
+
+  signin() {
+    let route = 'signin'
+    if (this.authService.isAuthenticated()) {
+      route = 'home';
+    }
+    this.rotuer.navigate([route]);
   }
 
 }
