573ee873b77ae20a14b544abc6cc63237772bedd
Tim Gundmann <tim@gundmann.dk>
2019-03-03 17:12:26 +0100

    style fix

--- src/app/home/fade-in-animation.ts
+++ src/app/home/fade-in-animation.ts
@@ -11,14 +11,25 @@ export const fadeInAndOutForPlan =
 export const fadeInAndOutForRoute =
     trigger('fadeInAndOutForRoute', [
         transition('* => *', [
+            style({ position: 'relative' }),
+            query(':enter, :leave', [
+                style({
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    paddingRight: '15px',
+                    paddingLeft: '15px'
+                })
+            ]),
             query(':enter', style({ opacity: 0 }), { optional: true }),
             query(':leave', [
                 style({ opacity: 1 }),
                 animate('.5s', style({ opacity: 0 }))
-            ],  { optional: true }),
+            ], { optional: true }),
             query(':enter', [
                 style({ opacity: 0 }),
                 animate('.5s', style({ opacity: 1 }))
-            ],  { optional: true }),
+            ], { optional: true }),
         ])
     ]);
