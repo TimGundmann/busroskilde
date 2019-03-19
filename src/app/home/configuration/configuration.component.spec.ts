6eeda756e79ef533f350693fe4f4a6a81c6184e3
Tim Gundmann <tim@gundmann.dk>
2019-03-19 10:03:19 +0100

    test fixed

--- src/app/home/configuration/configuration.component.spec.ts
+++ src/app/home/configuration/configuration.component.spec.ts
@@ -1,16 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
+import { FormsModule } from '@angular/forms';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConfigurationComponent } from './configuration.component';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ConfigurationComponent', () => {
   let component: ConfigurationComponent;
   let fixture: ComponentFixture<ConfigurationComponent>;
 
+  class MockHttpClient {
+    get() {
+      return of();
+    }
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ConfigurationComponent ]
+      declarations: [
+        ConfigurationComponent
+      ],
+      imports: [
+        FormsModule,
+        JwBootstrapSwitchNg2Module,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: HttpClient, useClass: MockHttpClient },
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
