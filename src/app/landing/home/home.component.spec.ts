4d0f7fc1b633f0faa9aaf96dcaf8ad35767b5b3b
Tim Gundmann <tim@gundmann.dk>
2019-03-20 08:46:46 +0100

    test fixed

--- src/app/landing/home/home.component.spec.ts
+++ src/app/landing/home/home.component.spec.ts
@@ -1,16 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SignupComponent } from './../signup/signup.component';
+import { SigninComponent } from './../signin/signin.component';
+import { MessageComponent } from './../message/message.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { of } from 'rxjs';
+import { tokenGetter } from 'app/app.module';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  class MockHttpClient {
+    get() {
+      return of();
+    }
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [
+        SignupComponent,
+        MessageComponent,
+        SigninComponent,
+        SignupComponent,
+        HomeComponent
+      ],
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule,
+        JwtModule.forRoot({
+          config: {
+            tokenGetter: tokenGetter,
+            whitelistedDomains: ['gundmann.dk'],
+          }
+        }),
+      ],
+      providers: [
+        { provide: HttpClient, useClass: MockHttpClient },
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
